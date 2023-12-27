2ce75785892c19a6415043e3894fa87d21e79ac1
nt <150912655+milavskijvadim@users.noreply.github.com>
2023-12-28 02:00:02 +0700

    Create supreme-fiesta.swift

---
+++ supreme-fiesta.swift
@@ -0,0 +1,10 @@
+class User {
+    private $username;
+    private $password;
+    private $isLoggedIn;
+
+    public function __construct($username, $password) {
+        $this->username = $username;
+        $this->password = $password;
+        $this->isLoggedIn = false;
+    }
